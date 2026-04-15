0be0c7b9f5aab2d60e8781614288c8b826e17af6
662115029 <nuthathai_pool@cmu.ac.th>
2026-04-15 23:14:02 +0700

    fix

--- bobo-fastapi/main.py
+++ bobo-fastapi/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # Add this block
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://test-bobo.netlify.app"],
+    allow_origins=["https://662115029.github.io"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -88,7 +88,7 @@ def handle_message(event):
                             "action": {
                                 "type": "uri",
                                 "label": "ดูงาน",
-                                "uri": "https://test-bobo.netlify.app"
+                                "uri": "https://662115029.github.io/Bobo-Tour-Management/"
                             },
                             "style": "primary"
                         }
