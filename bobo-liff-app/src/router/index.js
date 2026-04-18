368f612b871a6025e857beca0660939fa84901a0
662115029 <nuthathai_pool@cmu.ac.th>
2026-04-18 21:34:53 +0700

    fix 404

--- bobo-liff-app/src/router/index.js
+++ bobo-liff-app/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import JobListView from '../views/JobListView.vue'
 import JobDetailView from '../views/JobDetailView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: HomeView },
     { path: '/jobs', component: JobListView },
