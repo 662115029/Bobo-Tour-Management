b07927978ada716410f8b4e448b2376e2c612f84
662115005 <julalak_khamluerit@cmu.ac.th>
2026-06-10 21:26:14 +0700

    feat: register availability route

--- bobo-liff-app/src/router/index.js
+++ bobo-liff-app/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import JobListView from '../views/JobListView.vue'
-import JobDetailView from '../views/JobDetailView.vue'
+import JobsView from '../views/JobsView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import LoginView from '../views/LoginView.vue'
 import ChangePinView from '../views/ChangePinView.vue'
+import AvailabilityView from '../views/AvailabilityView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -15,8 +15,8 @@ const router = createRouter({
     { path: '/register', component: RegisterView },
     { path: '/profile', component: ProfileView },
     { path: '/profile/change-pin', component: ChangePinView },
-    { path: '/jobs', component: JobListView },
-    { path: '/jobs/:id', component: JobDetailView },
+    { path: '/jobs', component: JobsView },
+    { path: '/availability', component: AvailabilityView },
   ]
 })
 
