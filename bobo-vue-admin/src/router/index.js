8b814d1c9831c1e2a44be1cb856e35275634cc9a
662115005 <julalak_khamluerit@cmu.ac.th>
2026-08-06 04:29:55 +0700

    chore(deploy): add production env and gh-pages deploy script for admin app

--- bobo-vue-admin/src/router/index.js
+++ bobo-vue-admin/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import AdminLayout from "../layouts/AdminLayout.vue";
 import Dashboard from "../views/DashboardView.vue";
 import Jobs from "../views/JobsView.vue";
@@ -57,7 +57,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
